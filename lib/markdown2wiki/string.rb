b69fef9f8cfd1971aa7c55b73e816aae5fcc598d
madeindjs <madeindjs@gmail.com>
2017-07-10 13:34:24 +0200

    Correct italics & strongs

--- lib/markdown2wiki/string.rb
+++ lib/markdown2wiki/string.rb
@@ -8,13 +8,13 @@ class String
 		wiki = self
 
 		# convert italic!
-		wiki.gsub!(/\*((?!\s)[^\*]+?)\*(?=[^\*|$]{1})/, '_\1_')
+		wiki.gsub!(/[^\*]\*(\w+)\*(\s|\.)/, ' _\1_\2')
+
+		# convert strong!
+		wiki.gsub!(/(\s|\.)\*\*(.+)\*\*(\s|\.)/, '\1*\2*\3')
 
 		# convert link!
 		wiki.gsub!(/\[(.*)\]\((.*)\)/, '"\1":\2')
-
-		# convert strong!
-		wiki.gsub!(/\*\*((?!\s)[^\*]+?)\*\*(?=[^\*|$]{1})/, '*\1*')
 
 		# convert block code
 		wiki.gsub!(/(~|`){3}([a-z]+)/, '<pre><code class="\2">')
@@ -43,68 +43,4 @@ class String
 		return wiki
 	end
 
-	# private
-
-	# 	wiki = self
-
-
-	# 	return self
-	# end
-
-	# 	# convert unordered_list!(space = nil)
-	# 	space ||= '\t'
-
-	# 	pattern = /^(#{space}*\*)/
-	# 	pattern =~ self
-
-	# 	if $1.nil?
-	# 		return self
-	# 	end
-
-	# 	symbol = '*'
-	# 	$1.scan(/\t/).size.to_i.times do
-	# 		symbol += '*'
-	# 	end
-
-	# 	self.gsub!(pattern, symbol)
-	# 	return self
-	# end
-
-	# 	# convert ordered_list!(space = nil)
-	# 	space ||= '\t'
-
-	# 	pattern = /^(#{space}*[0-9]+?\.)/
-	# 	pattern =~ self
-
-	# 	if $1.nil?
-	# 		return self
-	# 	end
-
-	# 	symbol = '#'
-	# 	$1.scan(/\t/).size.to_i.times do
-	# 		symbol += '#'
-	# 	end
-
-	# 	self.gsub!(pattern, symbol)
-	# 	return self
-	# end
-
-	# 	# convert link!
-	# 	/\[(.+?)\]\((.+?)( "(.+)")?\)/ =~ self
-
-	# 	if $1.nil?
-	# 		return self
-	# 	end
-	
-	# 	title = ''
-
-	# 	if !$4.nil?
-	# 		title = '(' + $4 + ')'
-	# 	end
-	
-	# 	link = '"' + $1 + title + '":' + $2
-	
-	# 	self.gsub!(/\[(.+?)\]\((.+?)( "(.+)")?\)/, link)
-	# 	return self
-	# end
 end
--- spec/markdown2wiki_spec.rb
+++ spec/markdown2wiki_spec.rb
@@ -100,8 +100,8 @@ eos
   end
 
   it "convert markdown" do
-    markdown        = "Hello, [Markdown2Wiki](https://github.com/madeindjs/markdown2wiki) is ~good~ **anwesome** *(and it's written in `Ruby`)*"
-    expected_result = "Hello, \"Markdown2Wiki\":https://github.com/madeindjs/markdown2wiki is -good- *anwesome* _(and it's written in @Ruby@)_"
+    markdown        = "Hello, [Markdown2Wiki](https://github.com/madeindjs/markdown2wiki) is ~good~ **anwesome** *(and it's written in `Ruby`)*."
+    expected_result = "Hello, \"Markdown2Wiki\":https://github.com/madeindjs/markdown2wiki is -good- *anwesome* _(and it's written in @Ruby@)_."
     expect(markdown.markdown_to_wiki).to eq(expected_result)
   end
 
